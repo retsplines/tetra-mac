6b4268d23564cbbfd11e4d5994359fd353d0a4d7
Ret <folf@retsplin.es>
2025-02-01 19:21:08 +0000

    Add: Finish off remaining elements for SYSINFO

--- src/pdu/downlink/sysinfo.rs
+++ src/pdu/downlink/sysinfo.rs
@@ -14,11 +14,11 @@ enum HyperframeOrCipherKey {
 impl Encodable for HyperframeOrCipherKey {
     fn encode(&self, builder: &mut Builder) {
         match self {
-            HyperframeOrCipherKey::Hyperframe { hyperframe_number: u32 @ hyperframe } => {
+            HyperframeOrCipherKey::Hyperframe { hyperframe_number: hyperframe } => {
                 builder.write_bool(false);
                 builder.write_int(*hyperframe, 16);
             },
-            HyperframeOrCipherKey::CipherKey { cck_id_or_key_version_number: u32 @ cck_id } => {
+            HyperframeOrCipherKey::CipherKey { cck_id_or_key_version_number: cck_id } => {
                 builder.write_bool(true);
                 builder.write_int(*cck_id, 16);
             }
@@ -28,14 +28,42 @@ impl Encodable for HyperframeOrCipherKey {
 
 type TSModeBitmap = [bool; 20];
 
+impl Encodable for TSModeBitmap {
+    fn encode(&self, builder: &mut Builder) {
+        for bit in self.iter() {
+            builder.write_bool(*bit);
+        }
+    }
+}
+
 enum Immediate {
     AlwaysRandomise,
-    AfterFrames(u32)
+    AfterFrames(u32),
+    Immediate
+}
+
+impl Encodable for Immediate {
+    fn encode(&self, builder: &mut Builder) {
+        match self {
+            Immediate::AlwaysRandomise => builder.write_int(0b0000, 4),
+            Immediate::Immediate => builder.write_int(0b1111, 4),
+            Immediate::AfterFrames(after) => builder.write_int(*after, 4)
+        }
+    }
 }
 
 enum TimeslotPointer {
     SameAsDownlink,
     InTimeslots(Timeslots)
+}
+
+impl Encodable for TimeslotPointer {
+    fn encode(&self, builder: &mut Builder) {
+        match self {
+            TimeslotPointer::SameAsDownlink => builder.write_int(0b0000, 4),
+            TimeslotPointer::InTimeslots(timeslots) => timeslots.encode(builder)
+        }
+    }
 }
 
 struct AccessCodeDefinition {
@@ -47,12 +75,55 @@ struct AccessCodeDefinition {
     minimum_priority: u32
 }
 
-enum OptionalField {
-    TSModeEvenMultiframe (TSModeBitmap),
-    TSModeOddMultiframe (TSModeBitmap),
-    DefaultAccessCodeA,
-    ExtendedServicesBroadcast
+impl Encodable for AccessCodeDefinition {
+    fn encode(&self, builder: &mut Builder) {
+        self.immediate.encode(builder);
+        builder.write_int(self.waiting_time_opportunities, 4);
+        builder.write_int(self.number_of_attempts, 4);
+        builder.write_bool(self.frame_length_x4);
+        self.timeslot.encode(builder);
+        builder.write_int(self.minimum_priority, 3);
+    }
 }
+
+struct ExtendedServicesBroadcast { }
+
+impl Encodable for ExtendedServicesBroadcast {
+    fn encode(&self, _builder: &mut Builder) {
+        unimplemented!("extended services broadcast is not yet supported");
+    }
+}
+
+enum OptionalField {
+    TSModeEvenMultiframe(TSModeBitmap),
+    TSModeOddMultiframe(TSModeBitmap),
+    DefaultAccessCodeA(AccessCodeDefinition),
+    ExtendedServicesBroadcast(ExtendedServicesBroadcast)
+}
+
+impl Encodable for OptionalField {
+    fn encode(&self, builder: &mut Builder) {
+        match self {
+            OptionalField::TSModeEvenMultiframe(bitmap) => {
+                builder.write_int(0b00, 2);
+                bitmap.encode(builder);
+            },
+            OptionalField::TSModeOddMultiframe(bitmap) => {
+                builder.write_int(0b01, 2);
+                bitmap.encode(builder);
+            },
+            OptionalField::DefaultAccessCodeA(access_code_definition) => {
+                builder.write_int(0b10, 2);
+                access_code_definition.encode(builder);
+            },
+            OptionalField::ExtendedServicesBroadcast(extended_services_broadcast) => {
+                builder.write_int(0b11, 2);
+                extended_services_broadcast.encode(builder);
+            }
+        }
+    }
+}
+
 
 #[derive(FromPrimitive, ToPrimitive)]
 enum NumberOfCommonSCCH {
@@ -108,4 +179,47 @@ impl Encodable for Sysinfo {
         self.hyperframe_or_cipher_key.encode(builder);
         self.optional_field.encode(builder);
     }
+}
+
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn it_encodes_correctly() {
+        let sysinfo = Sysinfo {
+            main_carrier: 0x123,
+            frequency_band: 0x1,
+            offset: Offset::NoOffset,
+            duplex_spacing: 0x1,
+            reverse: false,
+            number_of_common_scch: NumberOfCommonSCCH::Timeslot2,
+            rf_parameters: RFParameters {
+                ms_txpwr_max_cell: 0x1,
+                rxlev_access_min: 0x1,
+                access_parameter: 0x1,
+                radio_downlink_timeout: 0x1
+            },
+            hyperframe_or_cipher_key: HyperframeOrCipherKey::Hyperframe {
+                hyperframe_number: 0x1
+            },
+            optional_field: OptionalField::DefaultAccessCodeA(AccessCodeDefinition {
+                immediate: Immediate::AlwaysRandomise,
+                waiting_time_opportunities: 0,
+                number_of_attempts: 0,
+                frame_length_x4: false,
+                timeslot: TimeslotPointer::SameAsDownlink,
+                minimum_priority: 0,
+            })
+        };
+
+        let mut builder = Builder::new();
+        sysinfo.encode(&mut builder);
+        let result = builder.done();
+
+        dbg!(result);
+
+
+    }
+
 }
